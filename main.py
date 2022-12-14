00508283471ec36f3ffbaa3b0ddbf72bf24715fd
Ali-Abdal <109912475+Ali-Abdal@users.noreply.github.com>
2022-12-14 15:58:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-# write your code here
+
+#1
+
+person = {
+    'name': 'Abdal',
+    'age': 18,
+    'hobbies': ['reading', 'playing games', 'sleeping']
+}
+
+print(f" {person['name']} \n {person['age']}")
+
+#2
+
+person['age'] = 0
+person['country'] = 'Kuwait'
+print(person)
+print(len(person))
+
+#3
+
+person['hobbies'].append('designing')
+
+def check_hobbies(person):
+    if len(person) >= 3:
+        print(' you are amazing')
+    
+    else: print('try harder')
+
+check_hobbies(person)
